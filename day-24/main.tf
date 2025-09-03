0de55eeb737d609a5a69a7d4ce70463c8bdb1d6c
Vishnu T <46985936+vishnutommandru@users.noreply.github.com>
2025-09-02 19:20:19 -0500

    Update main.tf

--- day-24/main.tf
+++ day-24/main.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "sub1" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "sub2" {
+resource "aws_subnet" "sub3" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
